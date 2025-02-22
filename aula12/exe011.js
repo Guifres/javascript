9e3ff26889a847e0b8e2d33553142600fc5a543f
Guilherme Freitas <gui2010mg68@gmail.com>
2025-02-22 09:32:06 -0300

    aula012

---
+++ aula12/exe011.js
@@ -0,0 +1,10 @@
+var idade  = 80
+console.log(`Voçê tem ${idade} anos`)
+if (idade < 16) {
+    console.log('Menor de idade')
+} else if (idade < 18 || idade > 65 ) {
+        console.log('Voto opcional')
+} else { 
+    console.log('Voto Obrigatorio!')
+
+}
