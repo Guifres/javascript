3005f9ddb40f010462700637b5fde2e6c60731b6
Guilherme Freitas <gui2010mg68@gmail.com>
2025-02-25 16:39:22 -0300

    exe001 aula12

--- aula12exe/javaScript/script.js
+++ aula12exe/javaScript/script.js
@@ -3,5 +3,18 @@ function carregar(){
     var img = window.document.getElementById('imagem')
     var data = new Date()
     var hora = data.getHours()
-    msg.innerHTML = `agora são ${hora} Horas.`
+    msg.innerHTML = `Agora são ${hora} Horas.`
+    if (hora >= 0 && hora < 12) {
+        // Bom dia
+        img.src = 'imagens/foto-manha.png'
+        document.body.style.background = '#b8a28d'
+    } else if (hora >= 12 && hora <= 18) {
+        // Boa Tarde
+        img.src = 'imagens/foto-tarde.png'
+        document.body.style.background = '#b15e22'
+    } else {
+        //BOa noite 
+        img.src = 'imagens/Noite.png'
+        document.body.style.background = '#704968'
+    }
 }
