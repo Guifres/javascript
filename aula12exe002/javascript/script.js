d8cf020c5b742651f1bf26c9ebb15d766932234b
Guilherme Freitas <gui2010mg68@gmail.com>
2025-02-27 21:16:18 -0300

    aula15

---
+++ aula12exe002/javascript/script.js
@@ -0,0 +1,14 @@
+function verificar() {
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById('txtano')
+    var res = getElementById('res')
+    if (fano.value.langth == 0 || fano.value > ano) {
+        window.alert('[ERROR] Verifique os dados e tente novamente')
+    } else {
+        var fsex = document.getElementById('radsex')
+        var idade = ano - Number(fano.value)
+        var genero = ''
+        i
+    }
+}
