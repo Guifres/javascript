70a479490bb8ec3ab657424b97576dc2234b6f5d
Guilherme Freitas <gui2010mg68@gmail.com>
2025-03-13 21:53:13 -0300

    Update script.js

--- aula12exe002/javascript/script.js
+++ aula12exe002/javascript/script.js
@@ -2,14 +2,14 @@ function verificar() {
     var data = new Date()
     var ano = data.getFullYear()
     var fano = document.getElementById('txtano')
-    var res = document.getElementById('res')
-    if (fano.value.langth == 0 || Number(fano.value) > ano) {
+    var res = document.querySelector('div#res')
+    }if (fano.value.langth == 0 || Number(fano.value) > ano) {
         window.alert('[ERROR] Verifique os dados e tente novamente')
     } else {
         var fsex = document.getElementsByName('radsex')
         var idade = ano - Number(fano.value)
         var genero = ''
-        if (fsex[0].checked) {
+        }if (fsex[0].checked) {
             genero = 'Homem'
         }else if (fsex[1].checked){
             genero = 'Mulher'
