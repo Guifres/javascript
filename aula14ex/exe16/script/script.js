3445247106e9dae39cecd2b39f5261dd4d869bd5
Guilherme Freitas <gui2010mg68@gmail.com>
2025-03-22 19:21:29 -0300

    .

--- aula14ex/exe16/script/script.js
+++ aula14ex/exe16/script/script.js
@@ -1,1 +1,20 @@
-var ini = document.getElementById('inicio')
+function Contar() {
+    var ini = document.getElementById('txti')
+    var fim = document.getElementById('txtf')
+    var passo = document.getElementById('txtp')
+    var res = document.getElementById('res')
+
+    if ( ini.value.length == 0 || fim.value.langth == 0 || passo.value.langth == 0) {
+        window.alert('[ERROR] Faltam dados')
+    } else {
+        res.innerHTML = ('Contando: ')
+        var i = Number(ini.value)
+        var f = Number(fim.value)
+        var p = Number(passo.value)
+
+
+        for (var c = i; c <= f; c += p) {
+            res.innerHTML += `${c}`
+        }
+    }
+}
