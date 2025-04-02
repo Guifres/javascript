8f0fd573a7a16593b06174e846bdf184e13bcb99
Guilherme Freitas <gui2010mg68@gmail.com>
2025-04-01 22:05:25 -0300

    aula15

---
+++ aula15/ambiente.js
@@ -0,0 +1,6 @@
+let num = [10, 8, 6]
+num.sort()
+num.push
+console.log(num);
+console.log(`O vetor tem ${num.length} posições`);
+console.log(`O primeiro valor do vetor é ${num[0]}`);
---
+++ aula15/vetornatela.js
@@ -0,0 +1,5 @@
+let valores =[1,2,3,6,8,7,4,9]
+for (var pos = 0; pos < valores.length; pos++) {
+    console.log(`A posição ${pos} tem ovalor ${valores[pos]}`);
+    
+}
